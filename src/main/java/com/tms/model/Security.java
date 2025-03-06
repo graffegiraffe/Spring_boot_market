ae53501ec8f2bc832f42353234ca8db37067fc80
poviraevsergei <Siarhey.Pavirayeu@ventionteams.com>
2025-03-06 21:57:44 +0300

    Added Actuator

--- src/main/java/com/tms/model/Security.java
+++ src/main/java/com/tms/model/Security.java
@@ -2,10 +2,12 @@ package com.tms.model;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.sql.Timestamp;
 
+@Scope("prototype")
 @AllArgsConstructor
 @NoArgsConstructor
 @Component
--- src/main/java/com/tms/model/User.java
+++ src/main/java/com/tms/model/User.java
@@ -3,10 +3,12 @@ package com.tms.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.sql.Timestamp;
 
+@Scope("prototype")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
